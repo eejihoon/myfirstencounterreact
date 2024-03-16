8cbb988e102cadaf839170ad0068f01e9d900a4e
cocodori <moradulee@gmail.com>
2024-03-16 23:20:14 +0900

    state and lifecycle

---
+++ src/chapter06/NotificationList.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import Notification from './Notification';
+
+const reservedNotifications = [
+    {
+        id: 1,
+        message: "안녕하세요. 오늘 일정 알려드립니다."
+    },
+    {
+        id: 2,
+        message: "점심 시간입니다."
+    },
+    {
+        id: 3,
+        message: "곧 미팅이 시작됩니다."
+    },
+
+]
+
+let timer;
+
+export default class NotificationList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notifications: [],
+        };
+    }
+
+    componentDidMount() {
+        const { notifications } = this.state;
+        timer = setInterval(() => {
+            if (notifications.length < reservedNotifications.length) {
+                const index = notifications.length;
+                notifications.push(reservedNotifications[index]);
+                this.setState({
+                    notifications: notifications,
+                });
+            } else {
+                this.setState({
+                    notifications: []
+                }); 
+                clearInterval(timer);
+            }
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.notifications.map((notification) => {
+                    return <Notification 
+                        key={notification.id}
+                        id={notification.id}
+                        message={notification.message} 
+                    />
+                })}
+            </div>
+        )
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CommentList from './chapter05/CommentList';
+import NotificationList from './chapter06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <CommentList />
-  </React.StrictMode>
+  <NotificationList />
 )
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
