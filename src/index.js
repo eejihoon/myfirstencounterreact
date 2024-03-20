82558b7b299492102d67cf24b5028297df2f2b6e
cocodori <moradulee@gmail.com>
2024-03-20 23:00:05 +0900

    useState, useEffect, custom hook

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NotificationList from './chapter06/NotificationList';
+import Accommodate from './chapter07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <NotificationList />
+  <Accommodate />
 )
 
 // If you want to start measuring performance in your app, pass a function
