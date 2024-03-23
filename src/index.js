0cb5b5d2e979d966d42e236242721581fec401a7
eejihoon <moradulee@gmail.com>
2024-03-23 22:01:55 +0900

    context API

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import ProfileCard from "./chapter13/ProfileCard";
+import DarkOrLight from "./chapter14/DarkOrLight";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div>
-    <ProfileCard />
+    <DarkOrLight />
   </div>
 );
 
